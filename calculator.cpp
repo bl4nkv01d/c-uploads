22835bd0daaa7e015452f993bfe973df3b9aff78
Timothy Z <fluffymangames@gmail.com>
2017-07-16 22:40:56 -0400

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    
+    char again = 'R';
+    
+    while(again == 'r' || again == 'R'){
+
+    //calculator app
+    int num1, num2;
+    char opr;
+    cout << "enter the numbers you want to calculate" << endl;
+    cin >> num1 >> num2;
+    cout << "enter the operator you want to use" << endl;
+    cin >> opr;
+    cout << "Calculating... \n" << num1 << " " << opr << " " << num2 << " = ";
+    switch (opr) {
+        case '+':
+            cout << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << num1 * num2 << endl;
+            break;
+        case '/':
+            if(num2 != 0)
+            cout << num1 / num2 << endl;
+            else
+            cout << "invalid" << endl;
+            break;
+        default:
+            cout << "invalid token" << endl;
+            break;
+    }
+        cout << "\n Restarting... \n" << endl;
+    }
+    return 0;
+}
